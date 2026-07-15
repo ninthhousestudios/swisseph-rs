dc62b107ba55c02caef1e9d46ebe4ebcba9ad35f
josh <josh@ninthhouse.studio>
2026-07-15 17:46:10 -0400

    added a .is_nan() guard to function meff, as well as replacing .unwrap with .expect and an error message

--- src/corrections.rs
+++ src/corrections.rs
@@ -122,7 +122,10 @@ pub fn meff(r: f64) -> f64 {
     if r >= 1.0 {
         return 1.0;
     }
-    let i = EFF_ARR.iter().position(|(ri, _)| *ri <= r).expect("EFF_ARR (corrections.rs) corrupted; codebase badly misconfigured.");
+    let i = EFF_ARR
+        .iter()
+        .position(|(ri, _)| *ri <= r)
+        .expect("EFF_ARR (corrections.rs) corrupted; codebase badly misconfigured.");
     // since we return 1.0 is r >= 1.0, .position() will never return 0, thus i - 1 is safe from underflow
     let (r0, m0) = EFF_ARR[i - 1];
     let (r1, m1) = EFF_ARR[i];
